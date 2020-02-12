24eb8f4beb95e47e1e4e4c4c0d936b1639d39fd8
majafranz <majafranz99@gmail.com>
2020-02-12 00:10:57 +0000

    added camera resolution option

--- src/camera/capture.py
+++ src/camera/capture.py
@@ -2,13 +2,17 @@ from picamera import PiCamera
 from time import sleep
 import os
 from os import sys, path
+
 if __name__ == '__main__' and __package__ is None:
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
-from ..core.config import DATA_ROOT, PERSON, IMG_NUM, TIME_DISTANCE
+from core.config import DATA_ROOT, PERSON, IMG_NUM, TIME_DISTANCE, RESOLUTION
 
 if __name__ == '__main__':
     camera = PiCamera()
+
+    if RESOLUTION is not None:
+         camera.resolution = RESOLUTION
 
     foldername = str(PERSON).split('.')[1].capitalize()
     img_name_format = '{:06d}'
@@ -17,7 +21,7 @@ if __name__ == '__main__':
     # check if folders in path exist and if not create them
     os.makedirs(root_path, exist_ok=True)
 
-    img_name_format = '{:06d}'
+    img_name_format = '{:06d}.jpg'
 
     camera.start_preview()
     for i in range(IMG_NUM):
--- src/core/config.py
+++ src/core/config.py
@@ -2,15 +2,16 @@ from os import sys, path
 if __name__ == '__main__' and __package__ is None:
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
-from ..utils.enums import Person
+from utils.enums import Person
 
 ##### - CAPTURE CONFIG - #####
 PERSON = Person.MAJA
 IMG_NUM = 100
 TIME_DISTANCE = 1 # time between two captures in s
+RESOLUTION = None
 
 ##### - DATA CONFIG - #####
-DATA_ROOT = "../../data"
+DATA_ROOT = "../data"
 DATA_ANNOTATION = ""
 
 ##### - TRAINING CONFIG - #####
