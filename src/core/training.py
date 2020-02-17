1abb20f4a3347cd2a955202cb448f57e40dd43d9
majafranz <majafranz99@gmail.com>
2020-02-17 00:27:41 +0000

    computed average accuracy

--- src/core/training.py
+++ src/core/training.py
@@ -3,17 +3,18 @@ import sys
 import torch
 import torch.optim as optim
 import torch.nn as nn
-from sklearn.metrics import accuracy_score
+
 from src.utils.config import NUM_EPOCHS, NUM_CLASSES
 from src.utils.logger import trlog, logger
 from src.core.model import model
 from src.core.data import get_dataloaders
 
+
 def train(net, num_epochs, train_loader, test_loader, criterion, optimizer, device):
     start_time = time.time()
 
     for epoch in range(num_epochs):
-        trlog.info('Epoch: {:d}/{:d}'.format(epoch+1, num_epochs))
+        trlog.info('Epoch: {:d}/{:d}'.format(epoch + 1, num_epochs))
         net.train()
         fit(net, train_loader, criterion, optimizer, device, epoch, training=True)
         net.eval()
@@ -21,6 +22,7 @@ def train(net, num_epochs, train_loader, test_loader, criterion, optimizer, devi
 
 
 def fit(net, data_loader, criterion, optimizer, device, epoch, training=True):
+    acc_sum, acc_avg, loss_sum, loss_avg = 0.0, 0.0, 0.0, 0.0
 
     for i, (images, labels) in enumerate(data_loader):
         images = images.to(device)
@@ -44,13 +46,19 @@ def fit(net, data_loader, criterion, optimizer, device, epoch, training=True):
         correct = (predicted == labels).sum(dtype=torch.float32)
         accuracy = 100 * correct / len(labels)
 
+        acc_sum += accuracy.item()
+        loss_sum += loss.item()
+
         if training:
             formatstr = 'epoch {:2d}, step {:3d}: train loss={:.2f}, train accuracy={:.2f}%'
         else:
             formatstr = 'epoch {:2d}, step {:3d}: test loss={:.2f}, test accuracy={:.2f}%'
 
-        if not i % 10:
-            trlog.info(formatstr.format(epoch + 1, i, loss.item(), accuracy.item()))
+        if not i % 25 or i == epoch-1:
+            acc_avg = acc_sum / (i + 1)
+            loss_avg = loss_sum / (i + 1)
+
+            trlog.info(formatstr.format(epoch + 1, i, loss_avg, acc_avg))
 
 
 def start_training():
@@ -66,6 +74,7 @@ def start_training():
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
     train(net, NUM_EPOCHS, train_loader, test_loader, criterion, optimizer, device)
+
 
 if __name__ == '__main__':
     start_training()
--- src/utils/config.py
+++ src/utils/config.py
@@ -1,6 +1,5 @@
 from src.utils.enums import Person
 from os import path
-from torchvision import transforms as T
 
 ##### - CAPTURE CONFIG - #####
 PERSON = Person.MAJA
@@ -8,7 +7,7 @@ IMG_NUM = 100
 TIME_DISTANCE = 1  # time between two captures in s
 TIME_OFFSET = 10  # seconds before capturing starts
 RESOLUTION = None
-RESET = True
+RESET = False
 
 ##### - DATA CONFIG - #####
 DATA_ROOT = path.join('..', '..', 'data')  # done with join due to windows using \ and linux /
@@ -17,7 +16,7 @@ DATA_ANNOTATION = path.join(DATA_ROOT, "annotation.csv")
 IMAGE_SIZE = 244  # size of input image
 IMAGE_SCALE = 1.3  # how much of the image can be cut off
 
-BATCH_SIZE = 16
+BATCH_SIZE = 12
 NUM_WORKERS = 2
 SHUFFLE = True
 TRAIN_SPLIT = 0.8
