2dd7690dcd8fa5ba72f5596544d88d392b4c6ec9
majafranz <majafranz99@gmail.com>
2020-02-21 15:12:16 +0000

    some bug fixes in capture motion

--- src/camera/capture_motion.py
+++ src/camera/capture_motion.py
@@ -1,9 +1,14 @@
 from picamera import PiCamera
 from gpiozero import MotionSensor
 from os import path
+import sys
+
+if __name__ == '__main__' and __package__ is None:
+    sys.path.append(path.join(path.dirname(path.realpath(__file__)),'../..'))
 
 from src.camera.capture import get_offset_index
 from src.utils.config import DATA_ROOT
+from src.utils.logger import logger
 
 if __name__ == '__main__':
     pir = MotionSensor(4)
@@ -20,7 +25,9 @@ if __name__ == '__main__':
             pir.wait_for_motion()
             full_img_path = path.join(root_path, img_name_format.format(i))
             camera.capture(full_img_path)
+            logger.info('picture \'{:s}\' taken.'.format(full_img_path))
             pir.wait_for_no_motion()
+            i+=1
 
         except KeyboardInterrupt:
             break
