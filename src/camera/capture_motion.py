16ae1830b90fd1565fd236820433766caf2516d9
majafranz <majafranz99@gmail.com>
2020-02-23 14:27:58 +0000

    led only on if sensor is enabled

--- src/camera/capture_motion.py
+++ src/camera/capture_motion.py
@@ -8,11 +8,13 @@ if __name__ == '__main__' and __package__ is None:
 
 from src.camera.capture import get_offset_index, get_path_format
 from src.utils.logger import logger
+from src.camera.cameraLED import CameraLED
 
 if __name__ == '__main__':
     pir = MotionSensor(4)
     camera = PiCamera(sensor_mode=2)
-
+    led = CameraLED()
+    led.off()
     img_name_format, root_path = get_path_format('unlabeled')
     i = get_offset_index(root_path, img_name_format)
 
@@ -20,12 +22,13 @@ if __name__ == '__main__':
     while True:
         try:
             pir.wait_for_motion()
+            led.on()
             full_img_path = path.join(root_path, img_name_format.format(i))
             camera.capture(full_img_path)
             logger.info('picture \'{:s}\' taken.'.format(full_img_path))
             pir.wait_for_no_motion()
+            led.off()
             i+=1
 
         except KeyboardInterrupt:
             break
-
