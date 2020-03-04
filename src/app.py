c273f3d45cee6c05cf31f98208f4f96154beed2b
majafranz <majafranz99@gmail.com>
2020-03-04 02:11:27 +0000

    adapted app for tvm use instead of pytorch

--- src/app.py
+++ src/app.py
@@ -8,38 +8,40 @@ from PIL import Image
 import random
 from threading import Thread, Lock
 import tvm
+import numpy as np
 
 if __name__ == '__main__' and __package__ is None:
     sys.path.append(path.join(path.dirname(path.realpath(__file__)), '..'))
 
 from src.core.model import model
-from src.camera.capture import get_path_format
+from src.camera.capture import get_path_format, get_offset_index
 from src.utils.logger import logger
 from src.core.data import CustomTransforms
 from src.utils.enums import Person
 from src.optimize.load_model import load_module, input_name
 
-
 def process_output(output):
-    pass #    _, pred = torch.max(output, 1)
-    #    prediction = pred.item()
+    prediction = np.argmax(output, axis=1)[0]
 
-    #person = Person(prediction)
+    person = Person(prediction)
+    
+    if (person is not Person.NONE) and (person is not Person.OTHER):
+        sound_root = os.path.join('..', 'ressources', person.name.capitalize())
 
-    #sound_root = os.path.join('..', 'ressources', person.name.capitalize())
+        sound_sample = random.choice(os.listdir(sound_root))
 
-    #sound_sample = random.choice(os.listdir(sound_root))
+        play_sound(os.path.join(sound_root, sound_sample))
 
-    #play_sound(os.path.join(sound_root, sound_sample))
-
-    #print('{:s}, du stinkst.'.format(person.name.capitalize()))
+    print('{:s}, du stinkst.'.format(person.name.capitalize()))
 
 
 def play_sound(sound_path):
     os.system('play {:s}'.format(sound_path))
 
 
-def detect_person(net, i, num_save, tmp_img_root, img_format, transforms, pir, camera, lock):
+def detect_person(net, num_save, tmp_img_root, img_format, transforms, pir, camera, lock):
+    i = get_offset_index(tmp_img_root, img_format, pir.pin)
+    num_save += i
     while True:
         pir.wait_for_motion()
         lock.acquire()
@@ -51,10 +53,10 @@ def detect_person(net, i, num_save, tmp_img_root, img_format, transforms, pir, c
         img = transforms(img)
         img = img.unsqueeze(0)
 
-        net.set_input(input_name, tvm.nd.array(img.astype('float32')))  # net(img)
+        net.set_input(input_name, tvm.nd.array(img))  # net(img)
         net.run()
-        output = net.get_output(00)
-        print(output)
+        output = net.get_output(0).asnumpy()
+        print(output)        
         process_output(output)
 
         if i < num_save:
@@ -72,15 +74,13 @@ if __name__ == '__main__':
 
     camera = PiCamera(sensor_mode=2)
 
-    num_save = 100
-    i = 0
-
     tmp_img_root = path.join('..', 'TMP')
     os.makedirs(tmp_img_root, exist_ok=True)
 
     img_format, _ = get_path_format("")
     img_format = 'pin{}_' + img_format
-
+    
+    num_save = 5000
     lock = Lock()
 
     transforms = CustomTransforms(244, 1.3)
@@ -88,9 +88,9 @@ if __name__ == '__main__':
     module = load_module()
 
     thread0 = Thread(target=detect_person,
-                     args=(module, i, num_save, tmp_img_root, img_format, transforms, pir0, camera, lock,))
+                     args=(module, num_save, tmp_img_root, img_format, transforms, pir0, camera, lock,))
     thread1 = Thread(target=detect_person,
-                     args=(module, i, num_save, tmp_img_root, img_format, transforms, pir1, camera, lock,))
+                     args=(module, num_save, tmp_img_root, img_format, transforms, pir1, camera, lock,))
 
     thread0.start()
     thread1.start()
--- src/optimize/load_model.py
+++ src/optimize/load_model.py
@@ -1,12 +1,15 @@
 from tvm.contrib import graph_runtime
 import tvm
+from tvm import relay
+from os import path
 
 input_name = 'input.1'
 
 def load_module():
-    lib = tvm.runtime.load_module('../../models/net.tar')
-    graph = open('net.json').read()
-    params = bytearray(open('net.params', 'rb').read())
+    tvm_model_dir = path.join('..', 'models', 'tvm')
+    lib = tvm.runtime.load_module(path.join(tvm_model_dir,'net.tar'))
+    graph = open(path.join(tvm_model_dir, 'net.json')).read()
+    params = relay.load_param_dict(bytearray(open(path.join(tvm_model_dir, 'net.params'), 'rb').read()))
 
     ctx = tvm.cpu(0)
     module = graph_runtime.create(graph, lib, ctx)
